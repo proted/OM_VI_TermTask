d8474da90065d763c5828f2fce00bf9e290ff1d5
Burkov Alexey <32144882+AlexeyBurkov@users.noreply.github.com>
2021-04-24 20:08:55 +0300

    Update paired_sample.py

--- paired_sample.py
+++ paired_sample.py
@@ -18,7 +18,7 @@ def paired_sample_method(function: Callable, dim: int, work_step: Callable, test
     x = np.random.normal(0, 100, dim)
     for i in range(iteration_count):
         direction = np.random.uniform(-1, 1, dim)
-        direction /= np.linalg.norm(direction, x)
+        direction /= np.linalg.norm(direction, 2)
         t_step = test_step(i)
         x1, x2 = x - t_step * direction, x + t_step * direction
         x = x + work_step(i) * direction * np.sign(function(x1) - function(x2))
