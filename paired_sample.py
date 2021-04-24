573469772d66e0fb02efa71c0538f3ff5e4346d7
Burkov Alexey <32144882+AlexeyBurkov@users.noreply.github.com>
2021-04-24 19:58:26 +0300

    Update paired_sample.py

--- paired_sample.py
+++ paired_sample.py
@@ -2,11 +2,24 @@ import numpy as np
 from typing import Callable
 
 
+def boltzmann_step(k: int):
+    return 1 / np.log(1 + k)
+
+
+def hyperbolic_step(k: int):
+    return 1 / k
+
+
+def constant_step(k: int):
+    return 1
+
+
 def paired_sample_method(function: Callable, dim: int, work_step: Callable, test_step: Callable, iteration_count: int):
     x = np.random.normal(0, 100, dim)
     for i in range(iteration_count):
         direction = np.random.uniform(-1, 1, dim)
-        x = x + work_step(i) * direction * np.sign(
-            function(x - test_step(i) * direction) - function(x + test_step(i) * direction))
+        direction /= np.linalg.norm(direction, x)
+        t_step = test_step(i)
+        x1, x2 = x - t_step * direction, x + t_step * direction
+        x = x + work_step(i) * direction * np.sign(function(x1) - function(x2))
     return x
-        
