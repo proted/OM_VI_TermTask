aac78dcd946a3a0429d441950fd09bc16f6e0ff0
proted <protopop.eduardo@gmail.com>
2021-04-24 19:59:34 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -7,7 +7,8 @@ def function1(vec: np.ndarray):
         first_coords_sum += vec[i] ** 2
     nominator = np.sin(first_coords_sum - np.pi / 2) + np.log(1 + vec[len(vec) - 1] ** 2)
     denominator = 1 + first_coords_sum + vec[len(vec) - 1] ** 2
-    return nominator / denominator
+    function_value = nominator / denominator
+    return function_value
 
 
 def function2(vec: np.ndarray):
@@ -15,4 +16,18 @@ def function2(vec: np.ndarray):
 
 
 def function3(vec: np.ndarray):
-    pass
+    coords_sum = 0
+    for i in range(0, len(vec)):
+        coords_sum += vec[i] ** 2
+    nominator = np.log(1 + coords_sum) - 1
+    denominator = 1 + coords_sum
+    function_value = nominator / denominator
+    return function_value
+
+
+def function4(vec: np.ndarray):
+    coords_sum = 0
+    for i in range(0, len(vec)):
+        coords_sum += vec[i] ** 2
+    function_value = - 1 / (1 + coords_sum)
+    return function_value
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from simulated_annealing import boltzmann_annealing
-from functions import function1
+from functions import *
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
-print(boltzmann_annealing(2, function1, 1, 1000))
+data = list()
+exact_solution = np.array([0, 0])
+for i in range(0, 5000):
+    solution = boltzmann_annealing(2, function4, 0.5, 100)
+    data.append(np.linalg.norm(solution - exact_solution))
+
+# bins=[7 + i*0.01 for i in range(0, 500)]
+plt.hist(data, bins=100)
+plt.show()
