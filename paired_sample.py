47aa22d92c3107d954c937e60e5b0b97df82d8e1
Burkov Alexey <32144882+AlexeyBurkov@users.noreply.github.com>
2021-04-26 00:20:16 +0300

    Update paired_sample.py

--- paired_sample.py
+++ paired_sample.py
@@ -14,12 +14,17 @@ def constant_step(k: int):
     return 1
 
 
-def paired_sample_method(function: Callable, dim: int, work_step: Callable, test_step: Callable, iteration_count: int):
-    x = np.random.normal(0, 100, dim)
+def square_step(k: int):
+    return k**2
+
+
+def paired_sample_method(function: Callable, dim: int, initial_vec: np.ndarray, work_step_initial: float,
+                         test_step_initial: float, work_step: Callable, test_step: Callable, iteration_count: int):
+    x = initial_vec.copy()
     for i in range(1, iteration_count):
         direction = np.random.uniform(-1, 1, dim)
         direction /= np.linalg.norm(direction, 2)
-        t_step = test_step(i)
+        t_step = test_step_initial*test_step(i)
         x1, x2 = x - t_step * direction, x + t_step * direction
-        x = x + work_step(i) * direction * np.sign(function(x1) - function(x2))
+        x = x + work_step_initial * work_step(i) * direction * np.sign(function(x1) - function(x2))
     return x
