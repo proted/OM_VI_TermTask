fde2440f1ca208c171a3627c64f142140ce0af49
Burkov Alexey <32144882+AlexeyBurkov@users.noreply.github.com>
2021-04-26 00:22:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ for i in range(0, 300):
 # bins=[7 + i*0.01 for i in range(0, 500)]
 plt.hist(data, bins=50)
 plt.show()
+
+#if __name__ == '__main__':
+#    iterations = [2500]
+#    data = [list() for _ in range(len(iterations))]
+#    exact_solution = np.ndarray((10,), buffer=np.array([1.0, 4.0, -12.9, -0.33334, 7.8, 1.0, 0.1, 4, -11, 6.8]))
+#    for i in range(len(iterations)):
+#        for _ in range(300):
+#            init = np.random.normal(np.ndarray((10,), buffer=np.array([1.0, 4.0, -12.9, -0.33334, 7.8, 1.0, 0.1, 4, -11, 6.8])), 1, 10)
+#            solution = paired_sample_method(function5, 10, init, 6, 1, hyperbolic_step, hyperbolic_step, iterations[i])
+#            data[i].append(np.linalg.norm(init - exact_solution, 2)/np.linalg.norm(solution - exact_solution, 2))
+#        plt.hist(data[i], bins=80)
+#        plt.xlabel('||x_0 - x*||/||x_k-x*||')
+#        plt.title(f'Число итераций = {iterations[i]}')
+#        plt.show()
