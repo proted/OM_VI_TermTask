c18c614bfc3e9e09b6b37b4c556c16edb8d4ba86
Maxim A. Susorov <protopop.eduardo@gmail.com>
2021-04-26 03:01:42 +0300

    Added research

--- main.py
+++ main.py
@@ -4,27 +4,57 @@ from functions import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = list()
-exact_solution = np.array([0, 0])
-for i in range(0, 300):
-    # solution = boltzmann_annealing(2, function2, 0.5, 100)
-    solution = paired_sample_method(function2, 2, 1, 3, boltzmann_step, boltzmann_step, 2500)
-    data.append(np.linalg.norm(solution - exact_solution))
+
+def hist_mean(data: np.ndarray):
+    (n, bins, patches) = plt.hist(data, bins=100)
+    n_copy = n.copy()
+    n_copy.sort()
+    n_copy = np.flip(n_copy)
+    top_count_values = n_copy[0: 3]
+    top_value_values = list()
+    sum_n = 0
+    for i in top_count_values:
+        index_in_n = list(n).index(i)
+        sum_n += n[index_in_n]
+        val = list(bins)[index_in_n] * n[index_in_n]
+        top_value_values.append(val)
+    return np.array(top_value_values).mean() / sum_n
+
+
+def do_accuracy_distribution_research():
+    pass
+
+
+def do_avg_dist_to_sln_against_iter_count_research():
+    exact_solution_for_1_4 = np.array([0, 0])
+    exact_solution_for_5 = np.array([1, 1, 1, 1, 1])
+    x_value = range(1, 200)
+    y_value = list()
+    for iteration_count in x_value:
+        one_step_data = list()
+        for experiment in range(1, 300):
+            solution = boltzmann_annealing(5, function5, 1, iteration_count)
+            distance = np.linalg.norm(solution - exact_solution_for_5)
+            # if not distance > 10:
+            #    one_step_data.append(distance)
+            one_step_data.append(distance)
+        y_value.append(hist_mean(one_step_data))
+    ax = plt.figure().add_subplot()
+    # ax.set_yscale('log')
+    # ax.set_xscale('log')
+    ax.plot(x_value, y_value)
+    plt.show()
+
+
+do_avg_dist_to_sln_against_iter_count_research()
+
+# data = list()
+# exact_solution = np.array([1, 1, 1, 1, 1])
+# for i in range(0, 1000):
+#   solution = boltzmann_annealing(5, function5, 0.5, 400)
+#   solution = paired_sample_method(function5, 5, 1, 3, boltzmann_step, boltzmann_step, 1000)
+#    data.append(np.linalg.norm(solution - exact_solution))
 
 # bins=[7 + i*0.01 for i in range(0, 500)]
-plt.hist(data, bins=50)
-plt.show()
-
-#if __name__ == '__main__':
-#    iterations = [2500]
-#    data = [list() for _ in range(len(iterations))]
-#    exact_solution = np.ndarray((10,), buffer=np.array([1.0, 4.0, -12.9, -0.33334, 7.8, 1.0, 0.1, 4, -11, 6.8]))
-#    for i in range(len(iterations)):
-#        for _ in range(300):
-#            init = np.random.normal(np.ndarray((10,), buffer=np.array([1.0, 4.0, -12.9, -0.33334, 7.8, 1.0, 0.1, 4, -11, 6.8])), 1, 10)
-#            solution = paired_sample_method(function5, 10, init, 6, 1, hyperbolic_step, hyperbolic_step, iterations[i])
-#            data[i].append(np.linalg.norm(init - exact_solution, 2)/np.linalg.norm(solution - exact_solution, 2))
-#        plt.hist(data[i], bins=80)
-#        plt.xlabel('||x_0 - x*||/||x_k-x*||')
-#        plt.title(f'Число итераций = {iterations[i]}')
-#        plt.show()
+# plt.hist(data, bins=50)
+# plt.show()
